3e638b7ddb7d03e1a7cff8c67f9e854b2d28bb64
gx <gx@localhost.localdomain>
2022-03-26 17:07:11 +0800

    add powershell srcipt

---
+++ powershell/foreach.ps1
@@ -0,0 +1,11 @@
+﻿$Platforms = "windows", "linux"
+$Archs = "386", "amd64", "arm", "arm64"
+foreach  ($OS in $Platforms) {
+    foreach($ARCH in $Archs) {
+        if($OS -eq "windows" -and $ARCH -eq "arm64") {
+            # windows/arm64 is not support now, skip it!
+            continue
+        }
+        Write-Host $OS --- $ARCH
+    }
+}
